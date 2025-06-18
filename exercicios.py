253a8dd6948018713087f739f81237484a033bda
danilovieira37 <danilovieira37@icloud.com>
2025-06-18 20:02:00 -0300

    adicionando exercicio2

--- exercicios.py
+++ exercicios.py
@@ -1,7 +1,24 @@
 # ex. 1
 # 1. Calcular Média de Valores em uma Lista
-from typing import List
-def media_de_uma_lista(lista: List[float]) -> float:
-    return sum(lista) / len(lista)
-valores = [2, 2]
-print(media_de_uma_lista(valores))
+# from typing import List
+# def media_de_uma_lista(lista: List[float]) -> float:
+#     return sum(lista) / len(lista)
+# valores = [2, 2]
+# print(media_de_uma_lista(valores))
+
+# 2. Filtrar Dados Acima de um Limite
+# lista_filtrada: list = []
+# def filtrar_dados_acima_de_um_limite(lista: list, limite: int) -> list:
+#     for n in lista:
+#         if n > limite:
+#             lista_filtrada.append(n)
+#     return lista_filtrada
+# filtro: list = filtrar_dados_acima_de_um_limite([2,3,4,6,78,88,89], 5)
+# print(filtro)
+# correcao ex. 2
+def filtrar_acima_de(valores: list[float], limite: float) -> list[float]:
+    resultado = []
+    for valor in valores:
+        if valor > limite:
+            resultado.append(valor)
+    return resultado
